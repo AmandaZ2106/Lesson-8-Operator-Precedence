0eed45a5bdddd5f7c450bf90ef027d768551c848
AmandaZ2106 <romeogcruz@outlook.com>
2025-08-27 19:14:58 +0100

    Mean Value

---
+++ Mean_Value.py
@@ -0,0 +1,5 @@
+a=int(input("Please enter a value(number)"))
+b=int(input("Please enter a value(number)"))
+c=int(input("Please enter a value(number)"))
+mean=(a+b+c)/3
+print("The mean of the entered value is",mean)
