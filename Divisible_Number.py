f5bbe8fef5fb0ba95f31df21b2ccd2dad24685a3
AmandaZ2106 <romeogcruz@outlook.com>
2025-08-27 18:56:56 +0100

    Divisible Number

---
+++ Divisible_Number.py
@@ -0,0 +1,8 @@
+print("Please enter a number(Numerator):")
+Num=int(input())
+print("Please enter the denominator")
+Den=int(input())
+if Num%Den==0:
+    print("\n",Num,"is divisible by",Den)
+else:
+    print("\n",Num,"is not divisible by",Den)    
