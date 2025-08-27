fcb50b9091a097402868a7d3025f35580eab5e97
AmandaZ2106 <romeogcruz@outlook.com>
2025-08-27 18:48:00 +0100

    Operator Precedence

---
+++ Operator_Precedence.py
@@ -0,0 +1,13 @@
+a=7
+b=2
+c=4
+d=8
+e=0
+e=((a+b)**3)/5
+print("The answer to ((a+b)**3)/5 is",e)
+
+name="Ayden"
+if name=="Ayden" or name=="Amanda":
+    print("Welcome")
+else:
+    print("Goodbye")    
