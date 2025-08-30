c511eb963a0a129f6eecde46458058f839246b8b
AmandaZ2106 <romeogcruz@outlook.com>
2025-08-30 17:43:45 +0100

    Swap

---
+++ Project.py
@@ -0,0 +1,12 @@
+a=4
+b=2
+c=50
+print("The corect problem is 9(3+5(50*2)/4))")
+print("The incorrect version is 9(3+5((4*2)/50))")
+print("The numbers that need to be swapped are",a,b,c)
+a,b,c=c,a,b
+print("This is the right order",a,b,c)
+Ans=9*(3+5*((50*2)/4))
+print("BTW. The answer to this question is",Ans)
+
+
